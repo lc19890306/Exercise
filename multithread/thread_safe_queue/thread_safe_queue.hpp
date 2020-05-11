cf95e59620df8989682489d6d89b15144d01d2ea
Chang Liu <liu28333613@gmail.com>
2020-05-10 20:23:55 -0400

    Update thread_safe_queue.hpp
    type

--- multithread/thread_safe_queue/thread_safe_queue.hpp
+++ multithread/thread_safe_queue/thread_safe_queue.hpp
@@ -12,7 +12,7 @@ public:
         cond.notify_one(); // 一定要通知挂起等待的线程
     }
     T pop() {
-        unique_lock<mutex> lk(mtx); // 条件变量必须配合unique_lock使用，因为unique_lock有lock何unlock函数
+        unique_lock<mutex> lk(mtx); // 条件变量必须配合unique_lock使用，因为unique_lock有lock和unlock函数
         cond.wait(lk, [this]{ return !q.empty(); }); // 当predicate不满足的时候cond释放锁并挂起线程等待，只有当被notify且predicate满足时才重新获得锁继续执行
         const T res = q.front();
         q.pop();
