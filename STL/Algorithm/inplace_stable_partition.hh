ae3da3d4ea33c74028ff90b4d43e20131e9e0990
Chang Liu <liu28333613@gmail.com>
2020-06-23 23:08:11 -0400

    Update inplace_stable_partition.hh

--- STL/Algorithm/inplace_stable_partition.hh
+++ STL/Algorithm/inplace_stable_partition.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+// distance
 #include <algorithm>
 
 namespace Algo {
@@ -11,8 +12,8 @@ Iter inplace_stable_partition(Iter b, Iter e, Cmp pred) {
     auto m = b + distance(b, e) / 2;
     b = inplace_stable_partition(b, m, pred); // return the index partitioning the left sub-container
     e = inplace_stable_partition(m, e, pred); // return the index partitioning the right sub-container
-    rotate(b, m, e); // rotate (swap) the right partition of the left sub-container with the left partition of the right sub-container
-    return b + distance(m, e); // return the new index after rotation, beginning with the index partitioning the left sub-container while the distance is the length of the left partition of the right sub-container
+    return (rotate)(b, m, e); // rotate (swap) the right partition of the left sub-container with the left partition of the right sub-container
+    // return b + distance(m, e); // return the new index after rotation, beginning with the index partitioning the left sub-container while the distance is the length of the left partition of the right sub-container
 }
 
 }; // namespace Algo
