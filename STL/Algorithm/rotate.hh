6d8de0a7759599309f0b1d6326dee95d7e60bd45
Chang Liu <liu28333613@gmail.com>
2020-06-23 23:07:00 -0400

    Create rotate.hh

---
+++ STL/Algorithm/rotate.hh
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace Algo {
+
+    template<typename Iter>
+    Iter rotate(Iter b, Iter m, Iter e) {
+        if (b == m) return e;
+        if (m == e) return b;
+        auto r = m, w = b, n = b;
+        while (r != e) {
+            if (w == n) {
+                n = r;
+            }
+            Algo::iter_swap(w++, r++);
+        }
+        (rotate)(w, n, r); // 此处加括号阻止ADL，默认使用namespace内的函数，不加会触发ADL有可能会引起歧义无法编译
+        return w;
+    }
+
+}; // namespace Algo
