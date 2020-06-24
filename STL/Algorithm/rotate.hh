9709768edb9eed28a9bb7c06be4ea3db39c4b358
Chang Liu <liu28333613@gmail.com>
2020-06-23 23:22:02 -0400

    Update rotate.hh

--- STL/Algorithm/rotate.hh
+++ STL/Algorithm/rotate.hh
@@ -14,7 +14,7 @@ namespace Algo {
             Algo::iter_swap(w++, r++);
         }
         (rotate)(w, n, r); // 此处加括号阻止ADL，默认使用namespace内的函数，不加会触发ADL有可能会引起歧义无法编译
-        return w;
+        return w; // w最后指向的就是rotate之后原来的b现在所指向的位置，即b + (e - m)
     }
 
 }; // namespace Algo
