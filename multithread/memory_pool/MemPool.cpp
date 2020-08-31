e9e76b64f2315c1bd2494fe156ce827186b7c57d
Chang Liu <liu28333613@gmail.com>
2020-08-31 01:03:36 -0400

    Update MemPool.cpp

--- multithread/memory_pool/MemPool.cpp
+++ multithread/memory_pool/MemPool.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-template<typename T, int NumofObjects = 20>
+template<typename T, int NumOfObjects = 20>
 class MemPool {
 public:
     ~MemPool() {
@@ -28,7 +28,7 @@ private:
 
     //内存块结构体
     struct MemBlock {
-        FreeNode data[NumofObjects];
+        FreeNode data[NumOfObjects];
         MemBlock *pNext{};
     };
 
@@ -38,8 +38,8 @@ private:
 };
 
 //分配空闲的节点
-template<typename T, int NumofObjects>
-T *MemPool<T, NumofObjects>::alloc() {
+template<typename T, int NumOfObjects>
+T *MemPool<T, NumOfObjects>::alloc() {
     lock_guard<mutex> lk(mtx);
     //无空闲节点，申请新内存块
     if (!freeNodeHeader) {
@@ -48,10 +48,10 @@ T *MemPool<T, NumofObjects>::alloc() {
 
         freeNodeHeader = &newBlock->data[0];     //设置内存块的第一个节点为空闲节点链表的首节点
         //将内存块的其它节点串起来
-        for (int i = 1; i < NumofObjects; ++i) {
+        for (int i = 1; i < NumOfObjects; ++i) {
             newBlock->data[i - 1].pNext = &newBlock->data[i];
         }
-        newBlock->data[NumofObjects - 1].pNext = nullptr;
+        newBlock->data[NumOfObjects - 1].pNext = nullptr;
 
         //首次申请内存块
         if (!memBlockHeader) {
@@ -69,8 +69,8 @@ T *MemPool<T, NumofObjects>::alloc() {
 }
 
 //释放已经分配的节点
-template<typename T, int NumofObjects>
-void MemPool<T, NumofObjects>::free(T *p) {
+template<typename T, int NumOfObjects>
+void MemPool<T, NumOfObjects>::free(T *p) {
     lock_guard<mutex> lk(mtx);
     auto pNode = reinterpret_cast<FreeNode *>(p);
     pNode->pNext = freeNodeHeader;    //将释放的节点插入空闲节点头部
